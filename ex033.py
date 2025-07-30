05f787434bbfcc86317716a28336da69c28aab2a
PedroKlipel <pedroklipel56@gmail.com>
2025-07-30 15:32:51 -0300

    melhorei o ex033
    deixei o exercício mas completo é mais "simplificado"

--- ex033.py
+++ ex033.py
@@ -1,8 +1,8 @@
-ano = int(input("me fale o ano que vc está"))
-if ano % 4 == 0:
-    if ano % 400 == 0:
-        print("o ano {} é um ano bissexto".format(ano))
-    else:
-        print("o ano {} não é um ano bissexto".format(ano))
+from datetime import date
+ano = int(input("me fale o ano que vc quer analisar, sé quiser o seu ano tecle 0  " ))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print("O ano {} é bissexto".format(ano))
 else:
-    print("o ano {} não é um ano bissexto".format(ano))
+    print("O anos {} não é bissexto".format(ano))
