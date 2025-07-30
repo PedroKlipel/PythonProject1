f52f11f9099c867ee5d9f6b35e7790c536cb82fb
PedroKlipel <pedroklipel56@gmail.com>
2025-07-30 15:17:12 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -3,7 +3,4 @@ if viagem <= 200:
     f1 = viagem * 0.50
 else:
     f1 = viagem * 0.45
-if viagem <= 200:
-    print("a sua passagem custara {}".format(f1))
-else:
-    print("a sua passagem custara {}".format(f1))
+print("a sua passagem custara {}".format(f1))
