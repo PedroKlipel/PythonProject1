e50251de173290a725cb8a2379f87ae395b5889b
PedroKlipel <pedroklipel56@gmail.com>
2025-07-30 17:04:36 -0300

    colocando cores

--- ex035.py
+++ ex035.py
@@ -1,7 +1,9 @@
 salario = float(input("Digite o seu salario: "))
 if salario >= 1250:
     aumento = salario * (10 / 100)
-    print("O seu salario será de {}".format(salario + aumento))
+    novo = salario + aumento
+    print("O seu salario erá de \033[31m{:.2f}\033[m e agora será de \033[32m{:.2f}".format(salario, novo))
 if salario < 1250:
     aumento = salario * (15 / 100)
-    print("Os seu salario será de {}".format(salario + aumento))
+    novo = salario + aumento
+    print("Os seu salario é de \033[31m{:.2f}\033[m, mas agora será de \033[32m{:.2f}".format(salario, novo))
